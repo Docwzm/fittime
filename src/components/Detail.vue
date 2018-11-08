e273d7aa037d94c1f150d1c1bfba4524a6fa6a49
dongqiang.wang <dongqiang.wang@lifesense.com>
2018-11-08 19:25:54 +0800

    修改封面图片遮盖显示

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -159,12 +159,11 @@ export default {
   }
   .mask {
     position: absolute;
-    top:0;
-    left:0;
-    width: 100%;
+    top:50%;
+    left:50%;
+    transform:translate(-50%,-50%);
     height: 100%;
     img {
-      width: 100%;
       height: 100%;
       display: block;
     }
