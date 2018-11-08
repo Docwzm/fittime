c3fe6356d468f03454b0d1065c9bf17dbf23fca0
dongqiang.wang <dongqiang.wang@lifesense.com>
2018-11-08 10:23:49 +0800

    修改标题显示

--- src/main.js
+++ src/main.js
@@ -41,15 +41,11 @@ if(u.indexOf('Android') > -1 || u.indexOf('Linux') > -1){
   Vue.prototype.systemType = 'web'
 }
 
-LSJavascriptBridgeInit(()=>{
-  setApp('全部课程')
-})
-
 function setApp(title=''){
   // window.bus.$root.$emit('APP-CB')
-  // LSJavascriptBridgeInit(() => {
+  LSJavascriptBridgeInit(() => {
     setAppNavTitle(title)
-  // })
+  })
   //分享2.0
   // shareData(
   //   {
@@ -90,14 +86,15 @@ function setApp(title=''){
 // }
 //
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title;
+  // document.title = to.meta.title;
   // setApp(to.meta.title)
+  setApp(to.meta.title)
   next()
 })
 router.afterEach((to, from, next) => {
-  setTimeout(()=>{
-    setApp(to.meta.title)
-  },200)
+  // setTimeout(()=>{
+    // setApp(to.meta.title)
+  // },200)
 })
 
 window.bus = new Vue({
