ac613e2079ba5d82b2cd45161a2f8dd5e7ae7324
dongqiang.wang <dongqiang.wang@lifesense.com>
2018-11-20 14:43:05 +0800

    路由缓存

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -54,7 +54,7 @@ export default {
       _czc.push(["_trackEvent", "class_fitime_listing", "点击", item.courseKey]);
       item.hotCount += 1;
       // location.href = '/fittime/#/detail?id='+item.courseKey //安卓标题栏显示问题
-      this.$router.replace({ name: "detail", query: { id: item.courseKey } });
+      this.$router.push({ name: "detail", query: { id: item.courseKey } });
     }
   }
 };
