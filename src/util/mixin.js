7af0de37053205a60d631063bc4ebacb1b56d06c
dongqiang.wang <dongqiang.wang@lifesense.com>
2018-11-20 17:37:05 +0800

    修复标题显示

--- src/util/mixin.js
+++ src/util/mixin.js
@@ -6,5 +6,12 @@ export default {
         LSJavascriptBridgeInit(() => {
             setAppNavTitle(title);
         });
+    },
+    activated(){
+        //解决keepAlvie返回时  标题消失
+        let title = this.$route.meta.title;
+        LSJavascriptBridgeInit(() => {
+            setAppNavTitle(title);
+        });
     }
 }
