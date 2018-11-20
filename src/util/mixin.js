fe514853abfb0647cace2edac046859a9796b9c8
dongqiang.wang <dongqiang.wang@lifesense.com>
2018-11-20 17:32:46 +0800

    修复标题显示

--- src/util/mixin.js
+++ src/util/mixin.js
@@ -3,7 +3,6 @@ import { setAppNavTitle, LSJavascriptBridgeInit } from "@/util/appApi";
 export default {
     created() {
         let title = this.$route.meta.title;
-        console.log(title);
         LSJavascriptBridgeInit(() => {
             setAppNavTitle(title);
         });
