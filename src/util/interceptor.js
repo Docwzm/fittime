068d1144d5ee8aa439a9ed756c2d9dd8eace91b4
dongqiang.wang <dongqiang.wang@lifesense.com>
2018-12-28 10:23:45 +0800

    将includes方法替代为indexOf

--- src/util/interceptor.js
+++ src/util/interceptor.js
@@ -7,7 +7,7 @@ const httpLockArr = {} //请求锁，避免同一条请求重复发送
 
 Vue.http.interceptors.push(function (request, next) {
   let _url = request.url
-  if(!request.url.includes('.json')){
+  if(request.url.indexOf('.json')<0){
     request.url = Vue.prototype.base_url + request.url + `?appType=${6}&version=${getAppVersionFromUserAgent()||"3.6.5"}&systemType=${1}&requestId=${Vue.prototype._g.getuuid()}`
   }
   let lastRequest = httpLockArr[_url]
@@ -20,13 +20,12 @@ Vue.http.interceptors.push(function (request, next) {
   //请求头设置
   //request.headers.set('Content-Type', 'application/json;charset=utf-8')
   
-  Vue.$vux.loading.show({
-    text: '加载中...'
-  })
+  // Vue.$vux.loading.show({
+  //   text: '加载中...'
+  // })
   next(function (response) {
     httpLockArr[request.url] = null
-    Vue.$vux.loading.hide();
-    alert(JSON.stringify(response))
+    // Vue.$vux.loading.hide();
     if (response.ok) {
       let data = response.data
       if (data.code == 200) {
