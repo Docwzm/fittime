0ab4ab0d80a0319f058de4d08e094c2838e831cc
dongqiang.wang <dongqiang.wang@lifesense.com>
2019-01-04 14:02:40 +0800

    request 调整

---
+++ src/util/env.js
@@ -0,0 +1,13 @@
+const staticHostApiHost = () => {
+    var mHost = location.hostname
+    return {
+        'cdn.lifesense.com': 'https://sports.lifesense.com',
+        'static-qa.lifesense.com': 'https://sports-qa.lifesense.com',
+        'static-qa2.lifesense.com': 'https://sports-qa2.lifesense.com',
+        'localhost': 'https://sports-qa2.lifesense.com',
+    } [mHost] || 'https://sports.lifesense.com'
+}
+
+export {
+    staticHostApiHost
+}
--- src/util/request.js
+++ src/util/request.js
@@ -1,36 +1,52 @@
 import Vue from 'vue'
 import axios from 'axios'
+import uuid from 'node-uuid'
 import { getAppVersionFromUserAgent } from './appApi'
-// create an axios instance
+import { staticHostApiHost } from './env'
+
+let pending = []; //声明一个数组用于存储每个ajax请求的取消函数和ajax标识
+let cancelToken = axios.CancelToken;
+let removePending = (config) => {
+  for (let p in pending) {
+    if (pending[p].u === config.url + '&' + config.method) { //当当前请求在数组中存在时执行函数体
+      pending[p].f(); //执行取消操作
+      pending.splice(p, 1); //把这条记录从数组中移除
+    }
+  }
+}
+
 const service = axios.create({
-  baseURL: process.env.BUILD_URL // api 的 base_url
+  baseURL: staticHostApiHost() // api 的 base_url
   // timeout: 5000 // request timeout
 })
 
 // request interceptor
 service.interceptors.request.use(
   config => {
-    console.log(config)
-    // Do something before request is sent
-    // if (store.getters.token) {
-    //   // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-    //   config.headers['X-Token'] = getToken()
-    // }
-    config.url += `?appType=${6}&version=${getAppVersionFromUserAgent() || "3.6.5"}&systemType=${1}&requestId=${Vue.prototype._g.getuuid()}`
+    if(!config.params.noPending) {
+      //并发请求的不加锁 例如多图上传时
+      removePending(config);//在一个ajax发送前执行一下取消操作
+      config.cancelToken = new cancelToken((c) => {
+        // 这里的ajax标识我是用请求地址&请求方式拼接的字符串，当然你可以选择其他的一些方式
+        pending.push({ u: config.url + '&' + config.method, f: c });
+      });
+    }
+    config.url += `?appType=${6}&version=${getAppVersionFromUserAgent() || "3.6.5"}&systemType=${1}&requestId=${uuid.v1()}`
     return config
   },
   error => {
-    // Do something with request error
-    console.log(error) // for debug
     Promise.reject(error)
   }
 )
 
-// response interceptor
 service.interceptors.response.use(
   response => {
-    if (response.status == 200) {
-      return response.data
+    if (!response.config.params.noPending) {
+      removePending(response.config);
+    }
+    let res = response.data;
+    if (res.code == 200) {
+      return res
     } else {
       Vue.$vux.toast.show({
         text: JSON.stringify(response),
@@ -39,41 +55,7 @@ service.interceptors.response.use(
       return Promise.reject('error')
     }
   },
-  /**
-   * 下面的注释为通过在response里，自定义code来标示请求状态
-   * 当code返回如下情况则说明权限有问题，登出并返回到登录页
-   * 如想通过 xmlhttprequest 来状态码标识 逻辑可写在下面error中
-   * 以下代码均为样例，请结合自生需求加以修改，若不需要，则可删除
-   */
-  // response => {
-  //   const res = response.data
-  //   if (res.code !== 20000) {
-  //     Message({
-  //       message: res.message,
-  //       type: 'error',
-  //       duration: 5 * 1000
-  //     })
-  //     // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-  //     if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-  //       // 请自行在引入 MessageBox
-  //       // import { Message, MessageBox } from 'element-ui'
-  //       MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-  //         confirmButtonText: '重新登录',
-  //         cancelButtonText: '取消',
-  //         type: 'warning'
-  //       }).then(() => {
-  //         store.dispatch('FedLogOut').then(() => {
-  //           location.reload() // 为了重新实例化vue-router对象 避免bug
-  //         })
-  //       })
-  //     }
-  //     return Promise.reject('error')
-  //   } else {
-  //     return response.data
-  //   }
-  // },
   error => {
-    console.log('err' + error) // for debug
     Vue.$vux.toast.show({
       text: JSON.stringify(error),
       type: 'text'
