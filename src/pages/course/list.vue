15e0ce4b70853ac63c0e80580287b567997782e0
dongqiang.wang <dongqiang.wang@lifesense.com>
2019-01-15 18:30:14 +0800

    视频详情tst

--- src/pages/course/list.vue
+++ src/pages/course/list.vue
@@ -41,15 +41,16 @@ export default {
   },
   mounted() {
     LSJavascriptBridgeInit(() => {
-      // navTitleBridge({
-      //   title: "全部课程",
-      //   autoTopPadding: true,
-      //   topPadding: 0,
-      //   tintColorType: 2,
-      //   backButtonType: 1,
-      //   barLineHidden: false,
-      //   color: { red: 255, green: 255, blue: 255, alpha: 255 }
-      // });
+      navTitleBridge({
+        title: "全部课程",
+        autoResetToDefaultConfigWhtenOpenLink: true,
+        autoTopPadding: true,
+        topPadding: 0,
+        tintColorType: 2,
+        backButtonType: 1,
+        barLineHidden: false,
+        color: { red: 255, green: 255, blue: 255, alpha: 255 }
+      });
     });
   },
   mixins: [mixin],
