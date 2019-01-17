b22f5dc68572ff1a36746a6eb4a282c5b69c6483
dongqiang.wang <dongqiang.wang@lifesense.com>
2019-01-17 10:28:58 +0800

    mixin 删除

--- src/util/mixin.js
+++ src/util/mixin.js
@@ -1,12 +1,12 @@
 export default {
-    activated(){
-        let name = this.$route.name;
-        // let el = document.body || document.documentElement;
-        document.documentElement.scrollTop = this._d.domTopList[name];
-    },
-    deactivated() {
-        let name = this.$route.name;
-        let el = document.body || document.documentElement;
-        this._d.domTopList[name] = el.scrollTop;
-    }
+    // activated(){
+    //     let name = this.$route.name;
+    //     // let el = document.body || document.documentElement;
+    //     document.documentElement.scrollTop = this._d.domTopList[name];
+    // },
+    // deactivated() {
+    //     let name = this.$route.name;
+    //     let el = document.body || document.documentElement;
+    //     this._d.domTopList[name] = el.scrollTop;
+    // }
 }
--- src/util/staticData.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-let _d = {}
-_d.domTopList={}
-Vue.prototype._d = _d;
