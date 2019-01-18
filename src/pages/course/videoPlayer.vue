4b5dcad9db591c3536ea90aad3b36921706552a0
dongqiang.wang <dongqiang.wang@lifesense.com>
2019-01-18 19:52:06 +0800

    视频详情test

--- src/pages/course/videoPlayer.vue
+++ src/pages/course/videoPlayer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="player-wrap">
     <div class="video-wrap">
-      <video ref="myVideo" id="my-video" width="100%" height="100%" :class="'video-js vjs-big-play-centered'"
+      <video ref="myVideo" id="my-video" width="100%" height="100%" class="video-js vjs-big-play-centered"
         x5-video-player-type="h5" x5-video-player-fullscreen="true" x5-video-orientation="landscape"
         style="object-fit:fill"></video>
       <div class="poster-wrap" v-if="posterFlag">
