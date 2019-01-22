67842b0df3d33e0392943d324ea5d331dd924c34
zhiqiang.peng <zhiqiang.peng@lifesense.com>
2019-01-22 11:27:15 +0800

    左右滑动还有bug

--- src/pages/course/list.vue
+++ src/pages/course/list.vue
@@ -2,13 +2,17 @@
   <div class="list-wrap" ref="list">
     <div class="list-header">
       <div class="class-box">
-        <div v-for="item in classify" :key="item.id" :style="'width:'+eachWidth" class="class-cate" :class="currentCate == item.id ?'current-cate':''"
-          @click="handleCategoryClick(item.id)">{{item.title}}</div>
+        <div v-for="(item,index ) in classify" :key="item.id" :style="'width:'+eachWidth" class="class-cate" :class="currentCate == item.id ?'current-cate':''"
+          @click="handleCategoryClick(item.id,index)">{{item.title}}</div>
       </div>
     </div>
-    <div class="list-content" @touchstart="handleTouchStart" @touchmove="handleTouchMove">
-      <list-item v-for="item in list" :key="item.id" :data="item"></list-item>
-    </div>
+    <swiper class='swiper' height='100%' ref='swiper' :show-dots="false" @on-index-change="handleIndexChange" v-model="index">
+      <swiper-item class="black swiper-item" v-for="cls in classify" :key="cls.id" @touchstart="handleTouchStart" @touchmove="handleTouchMove">
+        <div class="list-content" ref='listContent'>
+          <list-item v-for="item in classifyStore[cls.id]" :key="item.id" :data="item"></list-item>
+        </div>
+      </swiper-item>
+    </swiper>
     <div class="loading-wrap" v-if="bottomLoading">{{bottom.words}}</div>
   </div>
 </template>
@@ -17,11 +21,12 @@
 import ListItem from "@/components/ListItem";
 import { curriculumPage, getClassify } from "@/api/course.js";
 import { navTitleBridge, LSJavascriptBridgeInit } from "@/util/jsBridge";
-
+import { Swiper,SwiperItem} from 'vux'
 export default {
   name: "courseList",
   data() {
     return {
+      index:0,
       currentCate: 1,
       classify: [],
       list: [],
@@ -35,7 +40,9 @@ export default {
     };
   },
   components: {
-    "list-item": ListItem
+    "list-item": ListItem,
+    Swiper,
+    SwiperItem,
   },
   created() {
     let { classify, page } = this;
@@ -57,21 +64,34 @@ export default {
   },
   methods: {
     //切换课程类别
-    handleCategoryClick(key) {
+    handleCategoryClick(key,index) {
       this.currentCate = key;
+      this.index = index;
       this.list = []
       this.actionGetCourseListByCate({ pageNum: 1, classifyId: parseInt(key) });
     },
+
+    //头部滑动
+    handleIndexChange(index){
+      let {classify} = this;
+      this.currentCate = classify[index].id
+      this.actionGetCourseListByCate({pageNum:1,classifyId:classify[index].id})
+    },
+
     //根据课程类型拉取列表
     actionGetCourseListByCate(data, cb) {
-      curriculumPage({ ...data, pageSize: 10 })
+      let self = this
+      let {classifyStore,classify} = this
+      curriculumPage({ ...data, pageSize: 20 })
         .then(res => {
           if (res.code === 200) {
-            const { data } = res;
-            // let arr = [...data.list,...data.list,...data.list]
-            this.list = this.list.concat(data.list);
+            //const { data } = res;
+            //this.list = this.list.concat(res.data.list);
+            classifyStore[data.classifyId] = classifyStore[data.classifyId].concat(res.data.list)
+            this.classifyStore = classifyStore
+            console.log(classifyStore)
             //根绝分类计算宽度
-            let catLen = this.list.length;
+            let catLen = this.classify.length;
             let fullWidth = 100;
             let eachWidth = fullWidth
             if(catLen <= 5){
@@ -81,14 +101,15 @@ export default {
             }
             eachWidth = eachWidth.toString()+"%"
             this.eachWidth = eachWidth
-            this.maxPage = data.maxPage;
+            this.maxPage = res.data.maxPage;
             this.page += 1;
+            this.$nextTick(()=>{
+              let contentHeight = parseInt(this.$refs.listContent[0].clientHeight)
+              self.$refs.swiper.xheight = contentHeight.toString()+"px"
+            })
           }
           cb && cb(res);
         })
-        .catch(err => {
-          cb && cb(err);
-        });
     },
     //获取课程分类
     actionGetClassify(tab) {
@@ -98,6 +119,12 @@ export default {
           tab ? (this.currentCate = tab) : (this.currentCate = res.data[0].id);
           //所有
           this.classify = res.data;
+          let classifyStore = {}
+          this.classify.forEach(item => {
+            classifyStore[item.id] = []
+          })
+          this.classify = res.data
+          this.classifyStore = classifyStore
           this.actionGetCourseListByCate({
             pageNum: page,
             classifyId: tab || res.data[0].id
@@ -111,7 +138,7 @@ export default {
     handleTouchMove(e) {
       //判断方向
       if (e.targetTouches[0].pageY < this.startY) {
-        let innerHeight = document.querySelector(".list-content").clientHeight;
+        let innerHeight = document.querySelector(".swiper-item").clientHeight;
         let scrollTop =
           document.documentElement.scrollTop ||
           window.pageYOffset ||
@@ -186,8 +213,12 @@ export default {
       }
     }
   }
-  .list-content {
+  .swiper{
     padding-top: 102px;
+    box-sizing: border-box;
+  }
+  .list-content {
+    //padding-top: 102px;
     padding-bottom: 40px;
   }
   .loading-wrap {
